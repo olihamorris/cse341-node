fa577a2b7accd1fafa490e77d621800bddd24ea0
Morris Oliha <niyemayo@gmail.com>
2026-03-09 01:16:14 +0100

    Update server structure and root route

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("CSE341 Contacts API");
-});
-
 const mongodb = require("./db/connect");
 
 const swaggerUi = require("swagger-ui-express");
@@ -13,15 +9,24 @@ const swaggerDocument = require("./swagger.json");
 
 const port = process.env.PORT || 3000;
 
+// Middleware
 app.use(express.json());
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("CSE341 Contacts API is running");
+});
+
+// Routes
 app.use("/contacts", require("./routes/contacts"));
 
+// Swagger docs
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Start server after DB connects
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
